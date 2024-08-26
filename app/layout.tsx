626508e149eba3f202dc8dbc0755b412e17c4bbb
Mihir <mihirkantho@gmail.com>
2024-08-26 20:01:04 +0600

    update development mode to production mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider 
        appearance = {{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
         baseTheme: dark,
         variables:{
           colorPrimary: "#3371ff",
